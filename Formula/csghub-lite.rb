248966f9c25d7228a2850a6378634df25e037b53
ganisback <370036720@qq.com>
2026-06-28 17:17:48 +0800

    Update Homebrew formula for v0.9.2

--- Formula/csghub-lite.rb
+++ Formula/csghub-lite.rb
@@ -1,30 +1,30 @@
 class CsghubLite < Formula
   desc "Lightweight tool for running LLMs locally with CSGHub platform"
   homepage "https://github.com/opencsgs/csglite"
-  version "0.8.102"
+  version "0.9.2"
   license "Apache-2.0"
 
   on_macos do
     on_arm do
       url "https://github.com/OpenCSGs/csglite/releases/download/v#{version}/csghub-lite_#{version}_darwin-arm64.tar.gz"
-      sha256 "73c03e73dcfd7e4f5a1de694418402caf4f616978d98465ad8628c8b8c8cf12e"
+      sha256 "94ff1e886ca9d5e4acc359a844ca0b937c0ad47dbb51187b96fe04ef3a68a5fc"
     end
 
     on_intel do
       url "https://github.com/OpenCSGs/csglite/releases/download/v#{version}/csghub-lite_#{version}_darwin-amd64.tar.gz"
-      sha256 "1e27fbe109c91725856079118118661d1f816fec9b88d24c8cee9b5c875b0ec8"
+      sha256 "3031e0478365cb20e05656f5d87dadce60c26239a1f536e051d1869b89bc04ad"
     end
   end
 
   on_linux do
     on_arm do
       url "https://github.com/OpenCSGs/csglite/releases/download/v#{version}/csghub-lite_#{version}_linux-arm64.tar.gz"
-      sha256 "7db0f0da652f79976260e76036ba6d2e8d293b98654bf9f0c12f0f059c7b5e7f"
+      sha256 "0d02ea657103ab81e81c495ea364ec3e2ab140f76940db9c639520166a00348d"
     end
 
     on_intel do
       url "https://github.com/OpenCSGs/csglite/releases/download/v#{version}/csghub-lite_#{version}_linux-amd64.tar.gz"
-      sha256 "937bc45c60c0b6f402bfd7622348194d91caecb8c40704387fd442818ca61612"
+      sha256 "995de80013de7dd0c58184801dc09bd93293c8ae27f79e03b3ffdfcfe1dabb28"
     end
   end
 
